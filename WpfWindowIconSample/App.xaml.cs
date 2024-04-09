35c506a99960d3d43c8753c1039c2baead6d8489
Nytmo <nytmo@hotmail.com>
2024-04-09 17:11:17 +0200

    initial
     Author:    Nytmo <nytmo@hotmail.com>

---
+++ WpfWindowIconSample/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfWindowIconSample
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		public App()
+		{
+			EventManager.RegisterClassHandler(typeof(Window), Window.LoadedEvent, new RoutedEventHandler((s, e) =>
+			{
+				if (s is Window win)
+					RefreshDpiAwareResourceDictionaries(win);
+			}));
+
+			EventManager.RegisterClassHandler(typeof(Window), Window.DpiChangedEvent, new DpiChangedEventHandler((s, e) =>
+			{
+				if (e.OriginalSource is Window win && e.OldDpi.DpiScaleY != e.NewDpi.DpiScaleY)
+					RefreshDpiAwareResourceDictionaries(win);
+			}));
+		}
+
+		private static void RefreshDpiAwareResourceDictionaries(Window window)
+		{
+			var dpiScale = System.Windows.Media.VisualTreeHelper.GetDpi(window);
+
+			var dpi = dpiScale.PixelsPerInchY switch
+			{
+				<= 96 => 96,
+				> 96 and <= 120 => 120,
+				> 120 and <= 144 => 144,
+				> 144 and <= 192 => 192,
+				> 192 and <= 288 => 288,
+				_ => 384,
+			};
+
+			window.Resources.MergedDictionaries.Clear();
+			Uri uri = new($"/Icon/Icon-{dpi}dpi.xaml", UriKind.RelativeOrAbsolute);
+			window.Resources.MergedDictionaries.Add(new ResourceDictionary
+			{
+				Source = uri,
+			});
+			window.Title = $"DPI: {dpi}";
+		}
+	}
+
+}
---
+++ WpfWindowIconSample/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWindowIconSample
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		public int Index { get; private set; } = 0;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.DataContext = this;
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Index = (Index + 1) % 2;
+
+			this.Resources.MergedDictionaries.Clear();
+			this.Resources.MergedDictionaries.Add(new ResourceDictionary
+			{
+				Source = new($"/Icon/Index-{Index}.xaml", UriKind.RelativeOrAbsolute),
+			});
+
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Index)));
+		}
+	}
+}
